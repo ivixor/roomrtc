023279db9e8efe9f85c6d8abf4a6ba05aebd5273
ivixor <gfscoil@gmail.com>
2015-08-05 01:54:50 +0300

    Cleaned working dir

--- app.js
+++ app.js
@@ -5,29 +5,12 @@ var express = require('express')
 ,	path = require('path')
 ,	streams = require('./app/streams.js')();
 
-var favicon = require('serve-favicon')
-,	logger = require('morgan')
-,	methodOverride = require('method-override')
-,	bodyParser = require('body-parser')
-,	errorHandler = require('errorhandler');
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(favicon(__dirname + '/public/images/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(methodOverride());
-app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(errorHandler());
-}
 
 // routing
 require('./app/routes.js')(app, streams);
